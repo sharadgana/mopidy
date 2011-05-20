e669d8d69508ffee091c67ed55557a694470dda2
Thomas Adamcik <adamcik@samfundet.no>
2011-05-20 21:04:31 +0200

    Update roap plugin

--- mopidy/outputs/roap.py
+++ mopidy/outputs/roap.py
@@ -5,8 +5,9 @@ class ROAPOutput(BaseOutput):
     def describe_bin(self):
         return 'apexsink name=roap'
 
-    def modify_bin(self, output):
-        self.set_properties(output.get_by_name('roap'), {
+    def modify_bin(self):
+        roap = self.bin.get_by_name('roap')
+        self.set_properties(roap, {
             u'host': settings.ROAP_OUTPUT_SERVER,
             u'port': settings.ROAP_OUTPUT_PORT
         })
